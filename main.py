ad1352a2f9b30912e35316c1c87dae6006319543
Icindalis <kondrashin2007@outlook.com>
2024-10-15 21:25:54 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from matrix import user_input
+
+
 def main():
-    ...
+    # user_input возвращает список с вложенными списками
+    matrix = user_input()
+
 
 if __name__ == '__main__':
     main()
