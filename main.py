1665096a6b59efd07a0fe84ca5c88e03e44da828
Icindalis <kondrashin2007@outlook.com>
2024-10-20 13:37:02 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from matrix import user_input
+from min_last_element_sublist import find_sublist_with_min_last_element
 
 
 def main():
     # user_input возвращает список с вложенными списками
     matrix = user_input()
-
+    # 2
+    print(find_sublist_with_min_last_element(matrix))
 
 if __name__ == '__main__':
     main()
